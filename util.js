a919142c501d04915787655d17a5783d012f9117
Bjarki <bfr2@hi.is>
2018-11-22 07:42:44 +0000

    remove unused functions from util.js

--- util.js
+++ util.js
@@ -8,36 +8,6 @@
 
 var util = {
 
-
-// RANGES
-// ======
-
-clampRange: function(value, lowBound, highBound) {
-    if (value < lowBound) {
-	value = lowBound;
-    } else if (value > highBound) {
-	value = highBound;
-    }
-    return value;
-},
-
-wrapRange: function(value, lowBound, highBound) {
-    while (value < lowBound) {
-	value += (highBound - lowBound);
-    }
-    while (value > highBound) {
-	value -= (highBound - lowBound);
-    }
-    return value;
-},
-
-isBetween: function(value, lowBound, highBound) {
-    if (value < lowBound) { return false; }
-    if (value > highBound) { return false; }
-    return true;
-},
-
-
 // RANDOMNESS
 // ==========
 
@@ -61,18 +31,6 @@ distSq: function(x1, y1, x2, y2) {
     return this.square(x2-x1) + this.square(y2-y1);
 },
 
-wrappedDistSq: function(x1, y1, x2, y2, xWrap, yWrap) {
-    var dx = Math.abs(x2-x1),
-	dy = Math.abs(y2-y1);
-    if (dx > xWrap/2) {
-	dx = xWrap - dx;
-    };
-    if (dy > yWrap/2) {
-	dy = yWrap - dy;
-    }
-    return this.square(dx) + this.square(dy);
-},
-
 // CANVAS OPS
 // ==========
 
@@ -87,12 +45,6 @@ strokeCircle: function (ctx, x, y, r) {
     ctx.beginPath();
     ctx.arc(x, y, r, 0, Math.PI * 2);
     ctx.stroke();
-},
-
-fillCircle: function (ctx, x, y, r) {
-    ctx.beginPath();
-    ctx.arc(x, y, r, 0, Math.PI * 2);
-    ctx.fill();
 },
 
 fillBox: function (ctx, x, y, w, h, style) {
