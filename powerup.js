7a56bf97082057d9419f9f7d3ee66146deb40daf
Bjarki <bfr2@hi.is>
2018-11-22 05:55:44 +0000

    Powerup file cleaned and fixed time lasting

--- powerup.js
+++ powerup.js
@@ -14,20 +14,19 @@ powerUp.prototype = new Entity();
 // Initial, inheritable, default values
 powerUp.prototype.cx;
 powerUp.prototype.cy;
-powerUp.prototype.velX = 0;
 powerUp.prototype.velY = 3;
-powerUp.prototype.isPowerON = false;
 powerUp.prototype.NOMINAL_GRAVITY = 0.12;
 powerUp.prototype.radius = 20;
-powerUp.prototype.color;
-powerUp.prototype.lifespan = 2000 / NOMINAL_UPDATE_INTERVAL;
+powerUp.prototype.lifespan = 5000 / NOMINAL_UPDATE_INTERVAL;
+
 
 powerUp.prototype.update = function (du) {
 	spatialManager.unregister(this);
 	if (this._isDeadNow) return entityManager.KILL_ME_NOW;
 
+	// Make the powerup disappear after some time.
 	this.lifespan -= du;
-    if (this.lifespan < 0 ) return entityManager.KILL_ME_NOW;
+  if (this.lifespan < 0 ) return entityManager.KILL_ME_NOW;
 
 	var prevY = this.cy;
 	var nextY = prevY + this.velY*du;
@@ -41,26 +40,6 @@ powerUp.prototype.update = function (du) {
 	spatialManager.register(this);
 };
 
-/*
-powerUp.prototype.getPower = function (nextX,nextY,radius) {
-
-	var prevXX = this.cx;
-	var prevYY = this.cy;
-	var nextX = prevXX + this.velX;
-	var nextY = prevYY + this.velY;
-
-
-	var PlayerDist = util.distSq(nextX, nextY, prevXX, prevYY);
-	var limit = util.square(20 + radius);
-
-	if(limit < PlayerDist){
-		powerUp.prototype.isPowerON = true;
-	}
-
-	return powerUp.prototype.isPowerON;
-};
-*/
-
 powerUp.prototype.isPowerUp = function() {
 	return true;
 }
@@ -70,7 +49,6 @@ powerUp.prototype.getRadius = function() {
 }
 
 powerUp.prototype.render = function (ctx) {
-	ctx.beginPath();
 	var powerUpIds = [g_sprites.arrow, g_sprites.powerupPotion, g_sprites.powerupOrb, g_sprites.duck, g_sprites.powerupRing, g_sprites.powerupShield, g_sprites.sword, g_sprites.grenade]
 	this.sprite = powerUpIds[this.powerUpId];
 	this.sprite.drawCentredAt(ctx, this.cx, this.cy, 0);
